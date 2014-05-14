34d051d3da963f308bb488e93943e504955c2074
Moritz Schäfer <mail@moritzs.de>
2014-05-14 15:49:07 +0200

    adding new udpServer. note:not working

---
+++ TechGI4/first/udpServer.c
@@ -0,0 +1,103 @@
+
+/*
+############################################################################
+#                                                                          #
+# Copyright TU-Berlin, 2011-2014                                           #
+# Die Weitergabe, Veröffentlichung etc. auch in Teilen ist nicht gestattet #
+#                                                                          #
+############################################################################
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define MAX_BUFFER_LENGTH 100
+
+void unpackData(unsigned char *buffer, unsigned int *a, unsigned int *b);
+int gcd ( int a, int b );
+int main(int argc, char *argv[])
+{
+    int sockfd;
+    struct sockaddr_in own_addr, client_addr; // connector's address information
+    struct hostent *he;
+    int udpPort;
+
+    printf("UDP server example\n\n");
+
+    if (argc != 2) {
+        fprintf(stderr,"Usage: tcpServer udpPort\n");
+        exit(1);
+    }
+
+    udpPort = atoi(argv[1]);
+
+    // first udp
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    if(sockfd == -1) {
+        fprintf(stderr, "Error creating socket\n");
+        return 1;
+    }
+
+    //Resolv hostname to IP Address
+    if ((he=gethostbyname("localhost")) == NULL) {  // get the host info
+        herror("gethostbyname");
+        exit(1);
+    }
+
+    own_addr.sin_family = AF_INET;
+    own_addr.sin_port = htons(udpPort);
+    own_addr.sin_addr = *((struct in_addr *)he->h_addr);
+
+    memset(own_addr.sin_zero, '\0', sizeof own_addr.sin_zero);
+    //bind socket
+    if(bind(sockfd, &own_addr, sizeof(own_addr)) == -1) {
+        fprintf(stderr, "Error creating socket\n");
+        return 1;
+    }
+
+    socklen_t client_addr_size = sizeof(struct sockaddr_in);
+
+    char buffer[4];
+
+    int n;
+    if((n=recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)&client_addr, client_addr_size)) != 4) {
+        fprintf(stderr, "Error receiving data. expected 4 bytes but got %d \n", n);
+        return 1;
+    }
+
+    // why was buffer *buffer before
+    unsigned int a,b;
+    unpackData(buffer, &a, &b);
+    printf("received: %d, %d; GGT: %d\n", a, b, gcd(a,b));
+
+    if( close(sockfd) == -1 ) {
+        fprintf(stderr, "Error closing socket.\n");
+    }
+
+    return 0;
+}
+
+void unpackData(unsigned char *buffer, unsigned int *a, unsigned int *b) {
+    /* ******************************************************************
+       TO BE DONE:  pack data
+     ******************************************************************* */
+    *a = (buffer[0]<<8) | buffer[1];
+    *b = (buffer[2]<<8) | buffer[3];
+}
+
+int gcd ( int a, int b ) {
+    int c;
+    while ( a != 0 ) {
+        c = a; a = b%a;  b = c;
+    }
+    return b;
+}
